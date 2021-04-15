eb931ea52d487cad58cc8caac983d5a36a78268e
Leif Goodwin <leif.s.goodwin@gmail.com>
2021-04-15 04:07:45 -0700

    Initial Commit

---
+++ MycoRecorder/MycoRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MycoRecorderApp.swift
+//  MycoRecorder
+//
+//  Created by Leif Goodwin on 15/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct MycoRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
